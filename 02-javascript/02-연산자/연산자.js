dcf3ba0653c20b1ac01aaba48c3d28ba422d1ad5
유다현 <yudahyun@yudahyeon-ui-MacBookPro.local>
2022-01-25 16:56:00 +0900

    과제물

---
+++ 02-javascript/02-연산자/연산자.js
@@ -0,0 +1,18 @@
+const year = 2022;
+let age = 1995;
+
+console.log("나는 만"+(year-age)+"살 입니다.");
+
+
+
+const myAge = year - age;
+let todayYear = 2022;
+
+
+console.log("나는"+(todayYear - myAge)+"년 에 태어났습니다.");
+
+
+
+
+
+
