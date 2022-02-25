2be2c1f89927ab992b6a64eeb973a4b1d428fba5
유다현 <yudahyun@yudahyeon-ui-MacBookPro.local>
2022-02-25 13:39:32 +0900

    복습

---
+++ 02-javascript/13-예외처리/연습-220225-02.js
@@ -0,0 +1,17 @@
+function random(n1,n2){
+    return parseInt(Math.random()* (n2 - n1 + 1)) + n1;
+}
+
+function getLuckyResult(){
+    return new Promise(function(resolve,reject){
+        setTimeout(()=>{
+            console.log("당신의 추첨 결과는?");
+            const lucky =random(1,9);
+            if(lucky %2 == 0){
+                resolve({msg:"당첨",a:1,b:2});
+            }else{
+                reject({msg:"꽝",d:5});
+            }
+        },1000)
+    });
+}
--- 02-javascript/14-비동기처리/02-에러객체.js
+++ 02-javascript/14-비동기처리/02-에러객체.js
@@ -3,4 +3,4 @@ console.log("에러이름"+ err.name);
 console.log("에러내용"+ err.massage);
 
 throw err;
-//console.log("프로그램 종료");
+console.log("프로그램 종료");
